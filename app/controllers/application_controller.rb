747c64656f86e27568d05206509001ae4dc1ac48
Pedro Monsalve <codeloopy@gmail.com>
2025-06-14 23:20:29 -0400

    - Fixed devise permitted params - Partilized the form - Updated UI some styles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     added_attrs = [
       :first_name, :last_name, :phone,
-      :linkedin_url, :github_url, :portfolio_url,
+      :linked_in_url, :github_url, :portfolio,
       :email, :password, :password_confirmation, :remember_me
     ]
 
