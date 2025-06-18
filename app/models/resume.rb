b344da500e157f4b72a415cd7a8b8cd97f862599
Pedro Monsalve <codeloopy@gmail.com>
2025-06-18 15:09:43 -0400

    public resume link

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -1,6 +1,8 @@
 class Resume < ApplicationRecord
   belongs_to :user
   has_rich_text :summary
+  validates :slug, presence: true, uniqueness: true
+  before_validation :generate_slug, on: :create
 
   has_many :experiences, dependent: :destroy
   accepts_nested_attributes_for :experiences, allow_destroy: true
@@ -17,14 +19,15 @@ class Resume < ApplicationRecord
   has_many :references, dependent: :destroy
   accepts_nested_attributes_for :references, allow_destroy: true
 
-  has_many :skills, dependent: :destroy
-  accepts_nested_attributes_for :skills, allow_destroy: true
-
-  has_many :education, dependent: :destroy
-  accepts_nested_attributes_for :education, allow_destroy: true
-
-  has_many :projects, dependent: :destroy
-  accepts_nested_attributes_for :projects, allow_destroy: true
-
   delegate :first_name, :last_name, :email, :phone, :linked_in_url, :github_url, :portfolio, to: :user, prefix: true
+
+  def to_param
+    slug
+  end
+
+  private
+
+  def generate_slug
+    self.slug ||= [ user.first_name.parameterize, SecureRandom.hex(2) ].join("-")
+  end
 end
