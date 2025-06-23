77824770392a706eb29dc10134496177e74a36e7
Pedro Monsalve <codeloopy@gmail.com>
2025-06-23 10:53:27 -0400

    - Added Resend for production mailer - Updated app name in the nav

--- Gemfile
+++ Gemfile
@@ -68,3 +68,5 @@ gem "grover"
 gem "dockerfile-rails", ">= 1.7", group: :development
 
 gem "aws-sdk-s3", "~> 1.190", require: false
+
+gem "resend", "~> 0.22.0"
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,14 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: "freeresumebuilderapp.com", protocol: "https" }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :resend
+
+  config.action_mailer.resend_settings = {
+    api_key: Rails.application.credentials.dig(:resend, :api_key)
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
