7caa05202b38f8babeeacbd3261248766097a64b
Pedro Monsalve <codeloopy@gmail.com>
2025-06-27 20:53:03 -0400

    google analytics

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,3 +5,15 @@ import "../assets/stylesheets/application.tailwind.css";
 
 import "trix"
 import "@rails/actiontext"
+
+document.addEventListener("turbo:load", () => {
+  let googleId = document.querySelector("meta[name='google-site-verification']");
+
+  if (googleId) {
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', googleId);
+  }
+});
