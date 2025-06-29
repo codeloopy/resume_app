88f7be01013f30978703c93250120061907b1ddd
Pedro Monsalve <codeloopy@gmail.com>
2025-06-28 20:16:22 -0400

    Sentry back working

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require "sentry-ruby"
+require "sentry-rails"
+
 Sentry.init do |config|
   config.dsn = "https://37e3e7f914dea837f69e0cc7d14570f8@o4507951856025600.ingest.us.sentry.io/4509578236002304"
   config.breadcrumbs_logger = [ :active_support_logger, :http_logger ]
