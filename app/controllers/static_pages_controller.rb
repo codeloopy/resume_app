faf529cce933e22dce53bdc689b24b8d1b30570f
Pedro Monsalve <codeloopy@gmail.com>
2025-07-26 21:09:39 -0600

    caching

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
+    # Set cache headers for the static landing page
+    expires_in 1.hour, public: true
+    fresh_when(etag: "static_pages_home_v1", last_modified: 1.hour.ago)
   end
 end
