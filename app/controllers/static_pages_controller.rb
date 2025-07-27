7d0335ab0444c21ad8d88d95a2598fd66977f17d
Pedro Monsalve <codeloopy@gmail.com>
2025-07-26 21:14:29 -0600

    caching

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,12 @@
 class StaticPagesController < ApplicationController
   def home
     # Set cache headers for the static landing page
+    # Set cache expiration to 1 hour and make it publicly cacheable
     expires_in 1.hour, public: true
+
+    # Use conditional GET with ETag and Last-Modified headers
+    # ETag is a version identifier for the content
+    # Last-Modified is set to 1 hour ago to match cache expiration
     fresh_when(etag: "static_pages_home_v1", last_modified: 1.hour.ago)
   end
 end
