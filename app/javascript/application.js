62634ead253ca08adc816481981bff2403d5961f
Pedro Monsalve <codeloopy@gmail.com>
2025-09-26 13:09:13 +0100

    cleaning up tailwind issue on prod - what a mess I made

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,6 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
-import "../assets/stylesheets/application.tailwind.css";
 
 import "trix"
 import "@rails/actiontext"
