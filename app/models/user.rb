98f98bdbd8940ea8fd04121372eb99e69cbd8c4f
Pedro Monsalve <codeloopy@gmail.com>
2026-02-27 16:20:21 -0500

    Log registered users out after an hr

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :timeoutable
 
   def self.create_guest
     # Generate a unique email that won't conflict
